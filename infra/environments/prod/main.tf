09cff9bb4b30caa75f1646a83a89d9a26f3c3e14
Mykyta Glushko <nikita.glushko@gmail.com>
2026-02-08 22:46:57 +0200

    fix: update migrate job command for src/ structure

--- infra/environments/prod/main.tf
+++ infra/environments/prod/main.tf
@@ -202,7 +202,7 @@ resource "google_cloud_run_v2_job" "migrate" {
 
       containers {
         image   = var.container_image
-        command = ["python", "manage.py", "migrate", "--noinput"]
+        command = ["python", "src/manage.py", "migrate", "--noinput"]
 
         resources {
           limits = {
